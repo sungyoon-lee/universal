d323f447a43764d01a13998a7cc483f3a2f90945
sungyoon-lee <30929338+sungyoon-lee@users.noreply.github.com>
2017-11-12 20:32:33 +0900

    Update universal_pert.py

--- python/universal_pert.py
+++ python/universal_pert.py
@@ -53,7 +53,7 @@ def universal_perturbation(dataset, f, grads, delta=0.2, max_iter_uni = np.inf, 
         print ('Starting pass number ', itr)
 
         # Go through the data set and compute the perturbation increments sequentially
-        for k in range(0, num_images):
+        for k in range(0, num_images): # use all the data till we get to enough fooling rate
             cur_img = dataset[k:(k+1), :, :, :]
 
             if int(np.argmax(np.array(f(cur_img)).flatten())) == int(np.argmax(np.array(f(cur_img+v)).flatten())):
